10dfd88c3f196190ca405968bf1024842759c46f
g18s3628 <36521304+g18s3628@users.noreply.github.com>
2023-07-10 23:14:21 +0200

    conversion.py

---
+++ conversion.py
@@ -0,0 +1,20 @@
+#Declare for all the variables 
+#Convert num1 to an integer using int() command
+#Convert num2 into a float using the float() command
+#Convert num3 into a string using the str() command 
+#Convert string1 into an integer using the int() command
+#Print all the variables on separate lines 
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+num1_int = int(num1)
+print(num1_int)
+num2_float = float(num2)
+print(num2_float)
+num3_string = str(num3)
+print(num3_string)
+string1_int = int(string1)
+print(string1_int)
